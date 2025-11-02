5ff2fd7771ab516001c40c08187759a45e1b489b
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2025-11-02 12:56:17 +0530

    fixed border issues in overview chart

--- speedtest-app/frontend/src/components/SpeedChart.jsx
+++ speedtest-app/frontend/src/components/SpeedChart.jsx
@@ -366,7 +366,7 @@ const SpeedChart = ({ data, title, type = 'points' }) => {
     })(),
     plot_bgcolor: 'transparent',
     paper_bgcolor: 'transparent',
-    margin: { l: 60, r: 30, t: 30, b: 160 },
+    margin: { l: 60, r: 30, t: 30, b: 200 },
     dragmode: 'zoom',
     hovermode: 'closest'
   }
